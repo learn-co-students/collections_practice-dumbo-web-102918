14be513fa9b65b60c9c34412432e3070f902db43
a lan <bluenowte97@gmail.com>
2018-11-07 13:04:21 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,15 @@
 
+# def sort_array_asc(array)
+#    array.sort
+#    #returns new array
+# end
+
 def sort_array_asc(array)
-   array.sort
-   #returns new array 
+  sorted = []
+  i = 0
+  while i < array.length - 1
+    sorted.push(yield [i] <=> array[i + 1])
+    i+= 1
+  end
+  sorted
 end
